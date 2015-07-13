0181622f2643c77ad92787d9ae2fb8bff37435e6
jiangping.tang <jiangping.tang@zhenai.com>
2015-07-13 17:08:35 +0800

    add first framework

---
+++ FRInstantMessageManager/FRInstantMessageManager/FRInstantMessageManager.h
@@ -0,0 +1,19 @@
+//
+//  FRInstantMessageManager.h
+//  FRInstantMessageManager
+//
+//  Created by Illidan.Tang on 15/7/13.
+//  Copyright (c) 2015年 Illidan.Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FRInstantMessageManager.
+FOUNDATION_EXPORT double FRInstantMessageManagerVersionNumber;
+
+//! Project version string for FRInstantMessageManager.
+FOUNDATION_EXPORT const unsigned char FRInstantMessageManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FRInstantMessageManager/PublicHeader.h>
+
+
